8bbbb584b9e8d1db94cf205f64168335348ad609
KubasuIvanSakwa <kubasuivansakwa23@gmail.com>
2024-09-14 14:28:17 +0300

    new branch

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,9 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect, useRef } from "react"
 import { useNavigate } from "react-router-dom"
 import logo from "../assets/logo.svg"
 import googleLogo from "../assets/googleLogo.svg"
 import { auth, GoogleAuthProvider, signInWithPopup, onAuthStateChanged } from './../../firebase-config';
+import { signInWithEmailAndPassword } from "firebase/auth";
 
 
 
@@ -18,6 +19,7 @@ function Login() {
         const unsubscribe = onAuthStateChanged(auth, (user) => {
             if (user) {
                 navigate('/home/marketplace')
+                console.log(user)
             } else {
                 navigate('/login')
             }
@@ -30,22 +32,25 @@ function Login() {
         try {
             const result = await signInWithPopup(auth, provider);
             const user = result.user;
-            localStorage.setItem('usertype', `${user}`)
-            console.log('Signed in user:', user);
+            localStorage.setItem('usertype', `${user.displayName}`)
+            console.log('Signed in user:', user.displayName);
         } catch (error) {
             console.error('Google Sign-In failed:', error);
         }
     };
 
 
+
+
     return (
         <section className="relative flex flex-col w-full text-white h-[100vh] justify-center items-center bg-[#23272F]">
             <section className="flex flex-col lg:border overflow-hidden relative p-3 h-[28rem] rounded-xl justify-center lg:w-[30%]">
                 <form
-                    className="flex flex-col gap-2 h-[10rem] items-center"
+                    className="flex flex-col gap-2 h-[10rem] items-center mb-3"
                 // action="#" 
                 // onSubmit={onLoginWithEmailAndPassword}
                 >
+                    <p className="text-red-600">{error}</p>
                     <input
                         type="email"
                         name="email"
@@ -62,6 +67,7 @@ function Login() {
                         onChange={(e) => {
                             setPassword(e.target.value)
                         }}
+                        minLength='6'
                         placeholder="Password"
                         className="bg-[#373d49] placeholder-[#cbccce]/80 w-[17rem] h-[2.1rem] p-1 text-lg outline-none text-[#f5f6f7] rounded-sm"
                         required
@@ -69,7 +75,19 @@ function Login() {
                     <div className="flex gap-3">
                         <button
                             className="bg-white/10 rounded-lg p-2 mt-4 w-[6rem] self-center shadow-lg text-white/80  hover:bg-white/50 hover:text-white"
-                            type="submit"
+                            onClick={(e) => {
+                                e.preventDefault()
+                                signInWithEmailAndPassword(auth, email, password)
+                                .catch((err) => {
+                                    console.log(err.code)
+                                    if(err.code == 'auth/invalid-email') {
+                                        setError('*invalid email')
+                                    }else {
+                                        setError('*invalid user credentials')
+                                    }
+                                })
+                                localStorage.setItem('usertype', email)
+                            }}
                         >
                             Login
                         </button>
@@ -86,7 +104,7 @@ function Login() {
                         </button>
                     </div>
                 </form>
-                <div className="flex gap-2 relative w-full items-center justify-center mt-[-0.3rem]">
+                <div className="flex gap-2 relative w-full items-center justify-center mt-[-0.1rem]">
                     <hr className="border-[#909192]/80 w-[30%]" />
                     <p className="text-[#909192]/80 text-xl">or</p>
                     <hr className="border-[#909192]/80 w-[30%]" />
